717e86b3b15e98c35b783e0dfec9df4649d5680d
Len Trinh <trinhvanlen555@gmail.com>
2023-11-21 21:46:27 +0700

    remove code comment

--- BackEnd/ApiClient/PunkApiClient.cs
+++ BackEnd/ApiClient/PunkApiClient.cs
@@ -11,17 +11,6 @@ namespace ApiClient
         public async Task<List<BeerDto>> GetBeers()
         {
             var url = "https://api.punkapi.com/v2/beers";
-            //var response = await _httpClient.GetAsync(url);
-            //response.EnsureSuccessStatusCode();
-
-            //var responseString = await response.Content.ReadAsStringAsync();
-
-            //if (string.IsNullOrEmpty(responseString))
-            //{
-            //    return new List<ArticleDto> ();
-            //}
-
-            //return JsonConvert.DeserializeObject<List<ArticleDto>>(responseString);
 
             return await GetAsync<List<BeerDto>>(url);
         }
